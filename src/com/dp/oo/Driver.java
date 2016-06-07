af3bdb95706007d9b555ad8f7bf0358370cb51b8
Yang, Xiao-Tong <xiaotong.yang@hp.com>
2016-06-07 18:12:54 +0800

    Polymorphism

---
+++ src/com/dp/oo/Driver.java
@@ -0,0 +1,32 @@
+package com.dp.oo;
+
+public class Driver {
+   private String name;
+   
+   public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Driver(String name){
+	   this.name = name;
+   }
+   public void drive(Car c){
+      c.go(new Address("shanghai")); // hidden and encapsulate
+   }
+   
+   public void drive(Car c,Address dest){
+      c.go(dest); // hidden and encapsulate
+   }
+   
+   /*
+    * if don't use polymophim, add a new vihecle, need to add new method for it
+    * public void drive(Plane c,Address dest){
+      c.go(dest); // hidden and encapsulate
+   }
+    */
+   public void drive(Vihecle c,Address dest){
+      c.go(dest); // hidden and encapsulate
+   }
+}
---
+++ src/com/dp/oo/Vihecle.java
@@ -0,0 +1,6 @@
+package com.dp.oo;
+
+public abstract class Vihecle {
+	   
+	public abstract void go(Address dest);
+}
