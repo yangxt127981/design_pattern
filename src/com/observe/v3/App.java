6a0bd59e7aa114bd0cb324b4003be0620f49167b
Yang, Xiao-Tong <xiaotong.yang@hp.com>
2016-06-16 18:21:20 +0800

    singleton cache for property file
    Change-Id: Ic49ab0b1ae1b5ab819a307998d43b946a279feb8

--- src/com/observe/v3/App.java
+++ src/com/observe/v3/App.java
@@ -14,14 +14,7 @@ public class App {
 //		new Thread(c).start();
 		
 		/*use properties file, extend improve greatly, add new listener, only need to add a new class and add class name in properties file*/
-		Properties props = new Properties();
-		try {
-			props.load(App.class.getClassLoader().getResourceAsStream("com/observe/v3/observer.properties"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String[] observers = props.getProperty("observers").split(",");
+		String[] observers = PropertyMgr.getProperty("observers").split(",");
 		//System.out.println(observers);
 		Child c = new Child();
 		for(int i=0; i<observers.length;i++){
---
+++ src/com/observe/v3/PropertyMgr.java
@@ -0,0 +1,20 @@
+package com.observe.v3;
+
+import java.io.IOException;
+import java.util.Properties;
+/*save file into cache memory, load file from memory cache instead of hard disk. 
+ * if change file, memory cache not refresh automatically singleton*/
+public class PropertyMgr {
+	private static Properties props = new Properties();
+    static{
+	   try {
+			props.load(App.class.getClassLoader().getResourceAsStream("com/observe/v3/observer.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+     public static String getProperty(String key){
+		
+	     return props.getProperty(key);
+     }
+}
