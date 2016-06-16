514c8838246554e12900e4c48101f19302f8f8f4
Yang, Xiao-Tong <xiaotong.yang@hp.com>
2016-06-16 17:52:37 +0800

    use properties to implement observer pattern
    Change-Id: Ie4ac15bb77cee9cbcf4793655ca2f1aad6c2e0d3

--- src/com/observe/v3/App.java
+++ src/com/observe/v3/App.java
@@ -1,14 +1,41 @@
 package com.observe.v3;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class App {
 
 	public static void main(String[] args) {
-		Dad d = new Dad();
+//		Dad d = new Dad();
+//		Child c = new Child();
+//		GrandFather gf = new GrandFather();
+//		c.addWakenUpListener(d);
+//		c.addWakenUpListener(gf);
+//		new Thread(c).start();
+		
+		/*use properties file, extend improve greatly, add new listener, only need to add a new class and add class name in properties file*/
+		Properties props = new Properties();
+		try {
+			props.load(App.class.getClassLoader().getResourceAsStream("com/observe/v3/observer.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String[] observers = props.getProperty("observers").split(",");
+		//System.out.println(observers);
 		Child c = new Child();
-		GrandFather gf = new GrandFather();
-		c.addWakenUpListener(d);
-		c.addWakenUpListener(gf);
+		for(int i=0; i<observers.length;i++){
+			
+			try {
+				c.addWakenUpListener((WakenUpListener)(Class.forName(observers[i]).newInstance()));
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		new Thread(c).start();
-	}
 
+	}
+    
+	
 }
